b761e785388ca08e5dabeccb3de9335ae05968b4
Ruben Diaz <jrubendiaz@Rubens-MacBook-Pro.local>
2018-02-19 22:38:08 +0000

    Feedback mejorado para el click en el hashtag

--- src/app/instagram/instagram.component.ts
+++ src/app/instagram/instagram.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InstagramService } from './instagram.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-instagram',
@@ -33,7 +34,7 @@ export class InstagramComponent implements OnInit {
       .subscribe(data => {
         this.instaposts.ohs = data;
         console.log(this.instaposts.ohs);
-      }); ;
+      });;
 
     this.instaService.getImageByHashtag('gcsummerchallenge')
       .subscribe(data => {
@@ -44,16 +45,35 @@ export class InstagramComponent implements OnInit {
 
   ngOnInit() { }
 
-  clipboard(text) {
+  onClick(event) {
+    let copied = document.querySelector('.copied');
+
+    copied.classList.remove('off');
+
+    let element = event.target;
+    let y = event.clientY - 85;
+    let x = event.clientX - (copied.clientWidth / 2);
+
+    copied.setAttribute('style', 'transform: translate(' + x + 'px,' + y + 'px)');
+
+    setTimeout(() => {
+      copied.classList.add('off');
+    }, 1000);
+
+  }
+
+  clipboard(text, e?) {
     const inputToCopy = document.createElement('input');
     inputToCopy.setAttribute('value', text);
     document.body.appendChild(inputToCopy);
     inputToCopy.select();
     document.execCommand('copy');
     document.body.removeChild(inputToCopy);
+
+    console.log(e);
   }
 
-  downloadImage(url,hashtag){
+  downloadImage(url, hashtag) {
     const download = document.createElement('a');
     download.setAttribute('href', url);
     download.setAttribute('download', 'foto.jpg');
@@ -63,10 +83,10 @@ export class InstagramComponent implements OnInit {
     if (hashtag === 'cluster') this.clusterDownloaded = true;
   }
 
-  copyShareText(hashtag){
+  copyShareText(hashtag) {
     const text = `
-    En el @gcsummerchallenge yo voto por ${hashtag} para el primer wod del clasificatorio online, ¿y tú? 
-    - 
+    En el @gcsummerchallenge yo voto por ${hashtag} para el primer wod del clasificatorio online, ¿y tú?
+    -
     Si quieres saber cómo hacerlo, entra en la web app del campeonato y sube tu foto. (https://app.gcsummerchallenge.com/instagram)
     `;
     if (hashtag === '#gcsummerohs') this.ohsCopied = true;
